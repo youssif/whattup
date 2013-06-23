ac73e8543a73386912ef149513341ddc0c830f01
Youssif <abdulyoussif@gmail.com>
2013-06-23 11:06:30 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whattup::Application.config.secret_token = '7936142d745ed6a2736db5876d303c3c464a40c3f65e9638190adf2ccd3fc5efca1f88693ed5437e5661d5fd588456adf37be67429774261d7b739c6945d3b5d'
